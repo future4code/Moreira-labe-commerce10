95a9ca5420a657bbf0964c097ff88c87a874564d
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-01-20 14:41:45 -0300

    filtragem

--- src/Components/Filtros.js
+++ src/Components/Filtros.js
@@ -19,23 +19,34 @@ margin-bottom: 20px;
 
 
 export default class Filtros extends Component {
-  state = {
-    pesquisaNome: "",
+  // state = {
+  //   // pesquisaNome: "",
+  // }
 
-  }
-
-  atualizarPesquisa = (e) => {
-    this.setState({ pesquisaNome: e.target.value })
-  }
+  // atualizarPesquisa = (e) => {
+  //   this.setState({ pesquisaNome: e.target.value })
+  // }
 
   render() {
+
+    // const listaFiltrada = this.props.listaImagem.filter((item) => {
+    //   return item.Nome.includes(this.state.pesquisaNome)
+    // })
+
     return (
+
+
+
       <ContainerFiltro>
         <h3>Filtros</h3>
         Valor mínimo: <Input type="number" />
         Valor máximo: <Input type="number" />
-        Busca por nome: <Input type="text" placeholder="Pesquisa" value={this.state.pesquisaNome} onChange={this.atualizarPesquisa} />
+
+        Busca por nome: <Input type="text" placeholder="Pesquisa" value={this.props.pesquisaNome} onChange={this.props.atualizar} />
+
       </ContainerFiltro>
+
+
     )
   }
 }
