a3e1e7886d25a88a102f58162ad1e8efee46d171
sergiohscl <sergiohscl2@gmail.com>
2022-01-21 15:21:50 -0300

    atualizações de Imagens

--- src/Data/Itens.js
+++ src/Data/Itens.js
@@ -4,6 +4,12 @@ import Imgs2 from "../imgCamiseta/Nasa.jpg";
 import Imgs3 from "../imgCamiseta/Nave.jpg";
 import Imgs4 from "../imgCamiseta/OnibusEspacial.jpg";
 import Imgs5 from "../imgCamiseta/SistemaSolar.jpg";
+import Imgs6 from "../imgCamiseta/blueNasa.jpg";
+import Imgs7 from "../imgCamiseta/blusaFemenina.jpg";
+import Imgs8 from "../imgCamiseta/boneNasa.jpg";
+import Imgs9 from "../imgCamiseta/camisaPolo.jpg";
+import Imgs10 from "../imgCamiseta/BlusaoCapuz.jpg";
+import Imgs11 from "../imgCamiseta/MeiasNsa.jpg";
 
 export const Itens = [
     {
@@ -20,7 +26,7 @@ export const Itens = [
     },
     {
       id: 3,
-      Nome: "Nasa",
+      Nome: "Nasa Branca",
       Valor: 89.90,
       img: Imgs2,
     },
@@ -41,5 +47,41 @@ export const Itens = [
       Nome: "Sistema Solar",
       Valor: 39.90,
       img: Imgs5,
+    },
+    {
+      id: 7,
+      Nome: "Nasa Azul",
+      Valor: 89.90,
+      img: Imgs6,
+    },
+    {
+      id: 8,
+      Nome: "Blusa Feminina",
+      Valor: 79.90,
+      img: Imgs7,
+    },
+    {
+      id: 9,
+      Nome: "Boné Nasa",
+      Valor: 89.90,
+      img: Imgs8,
+    },
+    {
+      id: 10,
+      Nome: "Polo Branca",
+      Valor: 99.90,
+      img: Imgs9,
+    },
+    {
+      id: 11,
+      Nome: "Blusa de Frio c/ capuz",
+      Valor: 110.90,
+      img: Imgs10,
+    },
+    {
+      id: 12,
+      Nome: "Meias Nasa",
+      Valor: 19.90,
+      img: Imgs11,
     },
 ]
